0b32fcd078eb20697fef6d10ba7f2f89807e23a5
Louis Stenger <louis.stenger@epfl.ch>
2019-05-29 11:06:12 +0200

    Basic IOManager Class
    Just adds the class really. Since we will need it later, one can expect
    cyclic dpendencies with other inclusions.
    Might have to add forward declarations for some objects here.

---
+++ include/iomanager.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file IOManager
+ */
+
+#ifndef SURMISE_IOMANAGER_HPP_
+#define SURMISE_IOMANAGER_HPP_
+
+#include <fstream>
+#include <string>
+#include "serrors.hpp"
+#include "stypes.hpp"
+
+class IOManager {
+    public:
+    protected:
+    private:
+        std::ofstream outstream_;
+};
+
+#endif // SURMISE_IOMANAGER_HPP_
