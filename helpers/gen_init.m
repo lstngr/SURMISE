1d20cebdce4179d09eed6f148c09ba0e47057392
Louis Stenger <louis.stenger@epfl.ch>
2019-06-03 11:17:43 +0200

    Add Larger Test Case

--- helpers/gen_init.m
+++ helpers/gen_init.m
@@ -3,15 +3,17 @@
 
 %% Basic parameters
 domsize = 100;
-npart = 10;
-dt = 1e-2;
+npart = 100;
+dt = 1e-4;
 epsilon = 0.1;
+theta = 0.4;
 maxiter = 1000;
 maxwtime = 60;
 extratime = 10;
+output_path = 'out/';
 
 %% Distribution parameters (custom, user-filled)
-sim_idx = 3;
+sim_idx = 2;
 simname = 'default';
 savepath= './';
 switch sim_idx
@@ -23,16 +25,20 @@ switch sim_idx
         [x,v,m] = ic_alluniform(npart,0,domsize,0,0,1,1);
         simname = 'largeuniform';
     case 2
+        npart = 1000000;
+        [x,v,m] = ic_alluniform(npart,0,domsize,0,0,0.01,0.1);
+        simname = 'vlargeuniform';
+    case 3
         npart = 100;
         vrad = 8;
         vstd = 2;
         [x,v,m] = ic_radial(npart,0,domsize,vrad,vstd,0.05,1);
         simname = 'medradial';
-    case 3
+    case 4
         npart=1;
         [x,v,m] = ic_alluniform(npart,0,domsize,0,0,1,1);
         simname = 'easy';
-    case 4
+    case 5
         npart=2;
         [x,v,m] = ic_alluniform(npart,0,domsize,0,0,1,1);
         simname = 'easytoo';
@@ -50,10 +56,13 @@ fprintf(fileID,['domsize=%.10f\n',...
                 'npart=%u\n',...
                 'tevol_dt=%.10f\n',...
                 'epsilon=%.6f\n',...
+                'theta=%.6f\n',...
                 'max_iter=%u\n',...
                 'walltime=%.10f\n',...
-                'extratime=%.10f\n'],...
-                domsize,npart,dt,epsilon,maxiter,maxwtime,extratime);
+                'extratime=%.10f\n',...
+                'output_path=%s\n'],...
+                domsize,npart,dt,epsilon,theta,maxiter,maxwtime,extratime,...
+                output_path);
 fclose(fileID);
 fileID = fopen(fileinit,'w');
 fprintf(fileID,'%.25f %.25f %.25f %.25f %.25f\n',data');
