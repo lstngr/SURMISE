3fb206f80a76b0a95a7c8af2c0d447c9fd0dcfe4
Louis Stenger <louis.stenger@epfl.ch>
2019-06-06 12:17:30 +0200

    Helpers Update
    Radial configuration is now initialized with profiles that have a
    physical reason to be so. Great way to put it. Good job. Excellent.

--- helpers/gen_init.m
+++ helpers/gen_init.m
@@ -4,7 +4,7 @@
 %% Basic parameters
 domsize = 100;
 npart = 100;
-dt = 1e-4;
+dt = 1e-2;
 epsilon = 0.1;
 theta = 0.4;
 maxiter = 1000;
@@ -13,7 +13,7 @@ extratime = 10;
 output_path = 'out/';
 
 %% Distribution parameters (custom, user-filled)
-sim_idx = 1;
+sim_idx = 4;
 simname = 'default';
 savepath= './';
 switch sim_idx
@@ -35,16 +35,25 @@ switch sim_idx
         [x,v,m] = ic_radial(npart,0,domsize,vrad,vstd,0.05,1);
         simname = 'medradial';
     case 4
-        npart=1;
-        [x,v,m] = ic_alluniform(npart,0,domsize,0,0,1,1);
-        simname = 'easy';
-    case 5
-        npart=2;
-        [x,v,m] = ic_alluniform(npart,0,domsize,0,0,1,1);
-        simname = 'easytoo';
+        npart = 1e3;
+        vrad = 1;
+        vstd = 0.25;
+        clustsz = 15; clustpos=25;
+        [x1,v1,m1] = ic_radial(npart/2,0,clustsz,vrad,vstd,0.001,0.1);
+        [x2,v2,m2] = ic_radial(npart/2,0,clustsz,vrad,vstd,0.001,0.1);
+        x1 = x1 - clustpos;
+        x2 = x2 + clustpos;
+        x = [x1;x2];
+        v = [v1;v2];
+        m = [m1;m2];
+        clear x1 x2 v1 v2 m1 m2;
+        simname = 'clusters';
 end
 % Expect columnwise storage
 data = [x v m];
+% m propto volume propto r^2 (2D)
+% a=v^2/r and a propto m/r
+% v propto sqrt(ra) propto sqrt(m) propto sqrt(r^2)=r
 
 %% Ouputs parameters
 fileconf = [savepath,simname,'.conf'];
--- helpers/ic_radial.m
+++ helpers/ic_radial.m
@@ -2,7 +2,7 @@ function [x,v,m] = ic_radial(sz,xl,xh,vrad,vstd,ml,mh)
 
 dx = (xh - xl)/2;
 r  = unifrnd(0,dx,sz,1);
-vr = normrnd(vrad,vstd,sz,1);
+vr = (vrad + normrnd(0,vstd,sz,1)).*r;
 a  = unifrnd(0,2*pi,sz,1);
 x  = [50,50] + cat(2,r .*cos(a), r .*sin(a));
 v  = cat(2,vr.*sin(a),-vr.*cos(a));
