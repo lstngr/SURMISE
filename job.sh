2729771f90ca5466b9cecd4813a162aaeb910f6f
Louis Stenger <louis.stenger@epfl.ch>
2019-06-09 09:56:18 +0200

    Timing and Debug Commands in the SLURM job file

--- job.sh
+++ job.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks=8
-#SBATCH --time=00:00:20
+#SBATCH --ntasks=4
+#SBATCH --time=00:03:00
 #SBATCH --partition=debug
 
 module load gcc/7.3.0 mvapich2 totalview
 
-tvscript -event_action "error=>display_backtrace -show_arguments -show_locals" -mpi SLURM -tasks ${SLURM_NTASKS} /scratch/stenger/SuRMISe/bin/surmise -a /scratch/stenger/SuRMISe/input/alluniform
+tvscript -event_action "error=>display_backtrace -show_arguments -show_locals" -mpi SLURM -tasks ${SLURM_NTASKS} bin/surmise -a input/clusters
+#time srun -N ${SLURM_NNODES} -n ${SLURM_NTASKS} /scratch/stenger/SuRMISe/bin/surmise /scratch/stenger/SuRMISe/input/clusters
