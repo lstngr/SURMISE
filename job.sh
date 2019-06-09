00eb3a6465130461025d46ecc6ab56f33f3ad8f2
Louis Stenger <louis.stenger@epfl.ch>
2019-06-09 16:28:20 +0200

    Add MPI_Barrier in IO Routines
    In some cases, the particle buffers used to send particles were cleared before
    the receiving processes finished reading it. Not sure why this was problematic.
    Also adds memory requirements in the job file (done when attempting to debug
    the previous problem).

--- job.sh
+++ job.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
+#SBATCH --ntasks=8
+#SBATCH --mem-per-cpu=1G
 #SBATCH --time=00:10:00
 #SBATCH --partition=debug
 #SBATCH --chdir=/scratch/stenger/SuRMISe
--- src/iomanager.cpp
+++ src/iomanager.cpp
@@ -52,7 +52,6 @@ SError IOManager::WriteOutput( const Simulation& sim ) {
     // Get Root of the tree
     Node* pptr(sim.tree_->GetRoot());
     pptr = sim.tree_->GetNextLeaf( pptr );
-    Node* tptr(sim.tree_->GetRoot());
     // PARTICLE PRINTING
     // Iterate over tree (GetNextLeaf) until found all papptricles.
     while( pptr!=NULL ) {
@@ -64,6 +63,7 @@ SError IOManager::WriteOutput( const Simulation& sim ) {
         pptr = sim.tree_->GetNextLeaf( pptr );
     }
     // TREE PRINTING
+    // Node* tptr(sim.tree_->GetRoot());
     // tree << tptr << "," << tptr->children_[0] << ","
     //     << tptr->children_[1] << ","
     //     << tptr->children_[2] << ","
@@ -281,6 +281,7 @@ SError IOManager::SyncLeafs( Simulation& sim ) {
             delete[] recv_buf;
         }
     }
+    MPI_Barrier( MPI_COMM_WORLD );
     delete[] bsizes;
     delete[] send_buf;
     delete[] send_req;
@@ -324,7 +325,7 @@ SError IOManager::GenerateConfig( const std::string& file ) {
     // Particle array is dynamically allocated and should match input file size.
     conf_.parts.reserve(conf_.npart);
     std::ifstream ifsp( ifile );
-    for( size_t ip(0); ip < conf_.npart; ip++  ) {
+    for( long ip(0); ip < conf_.npart; ip++  ) {
         conf_.parts.push_back( new Particle );
         conf_.parts.back()->id = ip;
         ifsp >> conf_.parts.back()->pos[0]
